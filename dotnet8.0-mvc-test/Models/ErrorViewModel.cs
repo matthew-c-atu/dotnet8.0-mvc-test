6e37108d4b04ac4a7bb3c77f0326a160e4114e94
Matthew Cunningham <matthew@Matthews-MacBook-Air.local>
2024-01-17 23:36:36 +0000

    initial commit

---
+++ dotnet8.0-mvc-test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet8._0_mvc_test.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
